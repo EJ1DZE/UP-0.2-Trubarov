fb8da4dabdfbaa0cd37cd6361fa2111c5d370bb9
EJ1DZE <trubarov.andrey@mail.ru>
2024-12-20 00:00:02 +0300

    Добавьте файлы проекта.

---
+++ UP 0.2 Trubarov/Pages/Sales.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UP_0._2_Trubarov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Sales.xaml
+    /// </summary>
+    public partial class Sales : Page
+    {
+        private readonly Master_PolEntities db;
+        public Sales(int partnerID)
+        {
+            InitializeComponent();
+
+            db = new Master_PolEntities();
+
+            var partnerSales = db.PartnerProducts.Where(p => p.partnerID == partnerID).ToList();
+
+            DataGridSales.ItemsSource = partnerSales;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DiscountPage());
+        }
+    }
+}
